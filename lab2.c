48f35f7318eb4d2602f53345bbfa3c3f559b785a
reynol-31 <reynol.23cs119@sode-edu.in>
2024-12-14 01:05:16 +0530

    Create lab2.c

---
+++ lab2.c
@@ -0,0 +1,55 @@
+Reader Process:
+
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdio.h>
+#define MAX_BUF 1024
+int main()
+{
+  int fd;
+  /* A temporary FIFO file is not created in reader */
+  char *myfifo = "/tmp/myfifo";
+  char buf[MAX_BUF];
+  /* Open the named pipe for reading */
+  fd = open(myfifo, O_RDONLY);
+  /* Read data from the FIFO */
+  read(fd, buf, MAX_BUF);
+  printf("Writer: %s\n", buf);
+  /* Close the FIFO */
+  close(fd);
+  return 0;
+}
+
+
+
+
+
+Writer Process:
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main()
+{
+  int fd;
+  char buf[1024];
+  /* Create the named pipe (FIFO) */
+  char *myfifo = "/tmp/myfifo";
+  mkfifo(myfifo, 0666);
+  printf("Run Reader process to read the FIFO File\n");
+  /* Open the named pipe for writing */
+  fd = open(myfifo, O_WRONLY);
+  /* Write data to the FIFO */
+  strcpy(buf, "Hello from Writer Process");
+  write(fd, buf, sizeof(buf));
+  /* Close the FIFO */
+  close(fd);
+  /* Remove the FIFO */
+  unlink(myfifo);
+  return 0;
+}
+
